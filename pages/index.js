6d025f33de630aeeb38bc30a925753cb2f1370c4
Rxckhar <russaf007@mail.ru>
2022-02-18 14:40:16 +0500

    Добавление конструкции сайта

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 export default function Index() {
   return(
-    <div>Главная страница</div>
+    <div className="site"> 
+    <div className="site__wrapper"> 
+    <div className="site__menu"> 
+      Меню
+    </div>
+    <div className="site__content"> 
+    <div className="content__slider"> 
+      Слайдер
+    </div>
+    </div>
+    </div>
+    </div>
   );
 }
