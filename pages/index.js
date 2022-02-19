91d3a85cef46f10d72047f880614efdae6d3a080
Сафин Руслан Мухсиддинович <pl8426@C201N4.pl1.edu>
2022-02-19 15:50:18 +0500

    save

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 export default function Index() {
-  return(
-    <div className="site"> 
-    <div className="site__wrapper"> 
-    <div className="site__menu"> 
-      Меню
+  return (
+    <div className="site">
+      <div className="site__wrapper">
+        <div className="site__menu">
+        <div className="timg"></div>
+          <div className="zagol">Halloween</div>
+          
+        </div>
+        <div className="site__content">
+          <div className="content__slider">
+            <div className="tikva"></div>
+            <div className="title">
+              UOOOO TRICK OR TREAT!!
+
+            </div>
+            
+              
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="site__content"> 
-    <div className="content__slider"> 
-      Слайдер
-    </div>
-    </div>
-    </div>
-    </div>
-  );
+  )
 }
